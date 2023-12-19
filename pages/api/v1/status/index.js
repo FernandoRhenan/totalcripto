b7b0c9f2a9495c9c9160f5bfb2c6860a142d7230
FernandoRhenan <fernandorhenan9@gmail.com>
2023-12-19 10:47:27 -0300

    finish endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,13 @@
 import database from "infra/database.js";
-import { version } from "react";
 
 async function status(request, response) {
+  const databaseName = process.env.POSTGRES_DB
   const serverVersion = await database.query("SHOW server_version;");
   const maxConnections = await database.query("SHOW max_connections;");
-  const openConnections = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'")
+  const openConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
   const updatedAt = new Date().toISOString();
 
   response.status(200).json({
@@ -13,7 +16,7 @@ async function status(request, response) {
       database: {
         version: serverVersion.rows[0].server_version,
         max_connections: parseInt(maxConnections.rows[0].max_connections),
-        open_connections: openConnections.rows[0].count
+        open_connections: openConnections.rows[0].count,
       },
     },
   });
