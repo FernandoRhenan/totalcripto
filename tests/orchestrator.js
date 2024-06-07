38a99b2e7bded077f76dcc10bbd04edf25f713be
FernandoRhenan <fernandorhenan9@gmail.com>
2024-06-07 12:14:35 -0300

    makes tests flow works

---
+++ tests/orchestrator.js
@@ -0,0 +1,21 @@
+import retry from 'async-retry'
+
+async function waitForAllServices() {
+
+  await waitForWebServer()
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage)
+
+    async function fetchStatusPage() {
+      const res = await fetch(process.env.APP_URL + '/api/v1/status')
+      await res.json()
+    }
+
+  }
+
+}
+
+export default {
+  waitForAllServices
+}
