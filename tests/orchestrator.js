56215fb9c508d3e5156c7e59daf0bf3e59ea2da8
FernandoRhenan <fernandorhenan9@gmail.com>
2024-06-07 12:37:44 -0300

    `tests` workflow with `async-retry` and `orchestrator.js`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,7 +5,10 @@ async function waitForAllServices() {
   await waitForWebServer()
 
   async function waitForWebServer() {
-    return retry(fetchStatusPage)
+
+    return retry(fetchStatusPage, {
+      retries: 100
+    })
 
     async function fetchStatusPage() {
       const res = await fetch(process.env.APP_URL + '/api/v1/status')
