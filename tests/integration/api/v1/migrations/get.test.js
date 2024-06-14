5231a7b66ee894940b8fa2320484c3040ff1fcef
Fernando Weber <101434984+FernandoRhenan@users.noreply.github.com>
2024-06-14 14:04:25 +0000

    Remove the `orchestrator` from within the test files and place it within `package.json`. Run once for each battery of tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,8 +1,6 @@
-import orchestrator from "tests/orchestrator.js";
 import database from "infra/database.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices()
   await database.query("drop schema public cascade; create schema public;");
 });
 
@@ -16,13 +14,4 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(jsonResponse)).toBeTruthy();
   expect(jsonResponse.length).toBeGreaterThan(0);
 
-  // Just to open a new connection with db e test if it will be closed.
-  const response_ = await fetch("http://localhost:3000/api/v1/migrations");
-
-  const statusResponse = await fetch("http://localhost:3000/api/v1/status");
-
-  const jsonStatusResponse = await statusResponse.json();
-
-  expect(statusResponse.status).toBe(200);
-  expect(jsonStatusResponse.services.database.open_connections).toBe(1);
 });
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,12 +1,11 @@
 import database from "infra/database.js";
-import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices()
   await database.query("drop schema public cascade; create schema public;");
 });
 
 test("POST to /api/v1/migrations should return 200", async () => {
+
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
@@ -29,10 +28,4 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(jsonResponse)).toBeTruthy();
   expect(jsonResponse2.length).toEqual(0);
 
-  const statusResponse = await fetch("http://localhost:3000/api/v1/status");
-
-  const jsonStatusResponse = await statusResponse.json();
-
-  expect(statusResponse.status).toBe(200);
-  expect(jsonStatusResponse.services.database.open_connections).toBe(1);
 });
